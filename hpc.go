d6e3bbd2451aec23e1fc438cd6ea1961fd05f484
Garrett <garrett.adams@onyxpoint.com>
2018-10-30 16:24:57 -0400

    Added script builder

--- hpc.go
+++ hpc.go
@@ -2,18 +2,41 @@ package hpc
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 )
 
 type Job struct {
-	cmd string
+	ScriptPath  string
+	NativeSpecs string
+	UID         int
 }
 
-func test() {
-	fmt.Println("Test")
+func BuildScript(cmd, pathToSave, filenameSuffix string) (err error, scriptPath string) {
+
+	uniqueID := strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10)
+
+	batchScriptFull := pathToSave + "/" + filenameSuffix + uniqueID + ".bash"
+	batchScript, scriptErr := os.Create(batchScriptFull)
+	if scriptErr != nil {
+		return fmt.Errorf("Unable to create Script file: %v", scriptErr), ""
+	}
+	fmt.Fprintf(batchScript, "#!/bin/bash\n")
+	fmt.Fprintf(batchScript, cmd)
+
+	chmodErr := os.Chmod(batchScriptFull, 0750)
+	if chmodErr != nil {
+		return fmt.Errorf("Could not set the script to be executable: %v", chmodErr), ""
+	}
+	batchScript.Close()
+
+	return nil, batchScriptFull
+
 }
+
+//Detect batch system func should be added
 
 func (j *Job) Run() {
-
-	fmt.Println(j.cmd)
-
+	fmt.Println("Run Function")
 }
